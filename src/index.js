0104dc6dfcd97e6f7b91b8434b3e525510acca7e
MKPLKN <15703708+MKPLKN@users.noreply.github.com>
2023-12-21 13:09:34 +0200

    Export create/delete methods for cores and drives

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const authenticate = require('./auth/index.js')
+const { createCore, deleteCore } = require('./utils/cores.js')
+const { createDrive, deleteDrive } = require('./utils/drives.js')
 const { initMasterComponents, getMasterComponents } = require('./utils/masterComponents.js')
 
-module.exports = { authenticate, initMasterComponents, getMasterComponents }
+module.exports = { authenticate, initMasterComponents, getMasterComponents, createCore, deleteCore, createDrive, deleteDrive }
