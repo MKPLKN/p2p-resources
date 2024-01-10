d8a21e342ea066a15036bc074889ac1cf5fa5278
MKPLKN <15703708+MKPLKN@users.noreply.github.com>
2024-01-10 15:54:17 +0200

    Export loadConfigs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const authenticate = require('./auth/index.js')
-const { setConfig, getConfig } = require('./utils/config.js')
+const { setConfig, getConfig, loadConfigs } = require('./utils/config.js')
 const { createCore, deleteCore } = require('./utils/cores.js')
 const { createDrive, deleteDrive } = require('./utils/drives.js')
 const { initMasterComponents, getMasterComponents } = require('./utils/masterComponents.js')
 
-module.exports = { authenticate, initMasterComponents, getMasterComponents, createCore, deleteCore, createDrive, deleteDrive, getConfig, setConfig }
+module.exports = { authenticate, initMasterComponents, getMasterComponents, createCore, deleteCore, createDrive, deleteDrive, getConfig, setConfig, loadConfigs }
