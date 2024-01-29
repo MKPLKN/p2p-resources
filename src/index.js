cd5da1ba2518f1bb7853a53078213fabfffba54e
MKPLKN <15703708+MKPLKN@users.noreply.github.com>
2024-01-29 13:04:32 +0200

    Export hyperbee utility function

--- src/index.js
+++ src/index.js
@@ -4,25 +4,33 @@ const { createCore, deleteCore } = require('./utils/cores.js')
 const { createDrive, deleteDrive } = require('./utils/drives.js')
 const { createNode, createSwarm, makeNode, makeSwarm, deleteResource } = require('./utils/nodes.js')
 const { initMasterComponents, getMasterComponents } = require('./utils/masterComponents.js')
+const { makeDatabase } = require('./utils/databases.js')
 
 module.exports = {
   authenticate,
   initMasterComponents,
   getMasterComponents,
 
-  // Hypercores
+  // Hypercore
   createCore,
   deleteCore,
 
-  // Hyperdrives
+  // Hyperbee
+  makeDatabase,
+
+  // Hyperdrive
   createDrive,
   deleteDrive,
 
-  // DHT/Swarm
+  // DHT
   createNode,
   makeNode,
+
+  // Swarm
   createSwarm,
   makeSwarm,
+
+  // Helper
   deleteResource,
 
   // Config
