9b9a0c125d59251c1088cce22286aaad0e63fc74
MKPLKN <15703708+MKPLKN@users.noreply.github.com>
2024-01-29 09:06:06 +0200

    Export dht/swarm utility functions

--- src/index.js
+++ src/index.js
@@ -2,6 +2,31 @@ const authenticate = require('./auth/index.js')
 const { setConfig, getConfig, loadConfigs } = require('./utils/config.js')
 const { createCore, deleteCore } = require('./utils/cores.js')
 const { createDrive, deleteDrive } = require('./utils/drives.js')
+const { createNode, createSwarm, makeNode, makeSwarm, deleteResource } = require('./utils/nodes.js')
 const { initMasterComponents, getMasterComponents } = require('./utils/masterComponents.js')
 
-module.exports = { authenticate, initMasterComponents, getMasterComponents, createCore, deleteCore, createDrive, deleteDrive, getConfig, setConfig, loadConfigs }
+module.exports = {
+  authenticate,
+  initMasterComponents,
+  getMasterComponents,
+
+  // Hypercores
+  createCore,
+  deleteCore,
+
+  // Hyperdrives
+  createDrive,
+  deleteDrive,
+
+  // DHT/Swarm
+  createNode,
+  makeNode,
+  createSwarm,
+  makeSwarm,
+  deleteResource,
+
+  // Config
+  getConfig,
+  setConfig,
+  loadConfigs
+}
